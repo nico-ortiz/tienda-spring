22a31dd743e8f7dcbd2755c9070207b0bb7441da
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-08-15 15:34:28 -0300

    Create Product entity

---
+++ src/main/java/com/tienda/project/model/Producto.java
@@ -0,0 +1,43 @@
+package com.tienda.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Producto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long codigoProducto;
+
+    private String nombre;
+
+    private String marca;
+
+    private Double costo;
+
+    private Double cantidadDisponible;
+
+    public Producto() {}
+
+	public Producto(Long codigoProducto, String nombre, String marca, Double costo, Double cantidadDisponible) {
+		this.codigoProducto = codigoProducto;
+		this.nombre = nombre;
+		this.marca = marca;
+		this.costo = costo;
+		this.cantidadDisponible = cantidadDisponible;
+	}
+
+	public Producto(String nombre, String marca, Double costo, Double cantidadDisponible) {
+		this.nombre = nombre;
+		this.marca = marca;
+		this.costo = costo;
+		this.cantidadDisponible = cantidadDisponible;
+	}
+}
