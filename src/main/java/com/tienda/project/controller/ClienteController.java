28434084a334055aa8d86a68a3e99a5b935ea602
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-08-16 15:20:57 -0300

    Create GET endpoint to get a list of clients

--- src/main/java/com/tienda/project/controller/ClienteController.java
+++ src/main/java/com/tienda/project/controller/ClienteController.java
@@ -1,7 +1,10 @@
 package com.tienda.project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +22,11 @@ public class ClienteController {
     public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
         return ResponseEntity.ok(clienteService.createCliente(cliente));
     }
+
+    @GetMapping("/clientes")
+    public ResponseEntity<List<Cliente>> getCliente() {
+        return ResponseEntity.ok(clienteService.getClientes());
+    }
+
+    
 }
