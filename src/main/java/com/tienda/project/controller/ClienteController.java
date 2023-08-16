034cf7c2a9daddde84accb1bb1246f4bb89daf8b
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-08-16 15:26:20 -0300

    Create DELETE endpoint to delete a client by id

--- src/main/java/com/tienda/project/controller/ClienteController.java
+++ src/main/java/com/tienda/project/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,11 @@ public class ClienteController {
 
     @GetMapping("/clientes/{idCliente}")
     public ResponseEntity<Cliente> getCliente(@PathVariable Long idCliente) {
-        return  ResponseEntity.ok(clienteService.getCliente(idCliente));
+        return ResponseEntity.ok(clienteService.getCliente(idCliente));
+    }
+
+    @DeleteMapping("/clientes/eliminar/{idCliente}")
+    public ResponseEntity<Cliente> deleteCliente(@PathVariable Long idCliente) {
+        return ResponseEntity.ok(clienteService.deleteCliente(idCliente));
     }
 }
