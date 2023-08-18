de31ecc6f9d1da8fd7c907fb11f656ab6b50f937
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-08-18 14:58:44 -0300

    Modify attributes properties

--- src/main/java/com/tienda/project/dao/IClienteRepository.java
+++ src/main/java/com/tienda/project/dao/IClienteRepository.java
@@ -1,11 +1,9 @@
 package com.tienda.project.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.tienda.project.model.Cliente;
 
-@Repository
 public interface IClienteRepository extends JpaRepository<Cliente, Long>{
     
 }
--- src/main/java/com/tienda/project/model/Producto.java
+++ src/main/java/com/tienda/project/model/Producto.java
@@ -1,9 +1,15 @@
 package com.tienda.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,6 +29,10 @@ public class Producto {
     private Double costo;
 
     private Double cantidadDisponible;
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "listaProductos")
+	private List<Venta> ventas = new ArrayList<>();
 
     public Producto() {}
 
