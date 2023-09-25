e3e555d0405f04a43a0dbf31a5be40117b2a4aa1
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-09-25 11:21:04 -0300

    Modify files. Ref #9

--- src/main/java/com/tienda/project/model/User.java
+++ src/main/java/com/tienda/project/model/User.java
@@ -1,12 +1,5 @@
 package com.tienda.project.model;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -20,7 +13,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class User implements UserDetails {
+public class User {
     
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -68,41 +61,4 @@ public class User implements UserDetails {
 		this.apellido = apellido;
 		this.dni = dni;
 	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority(role.name()));
-	}
-
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
-	}  
-
-
 }
