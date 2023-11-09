6956379ac7893d5a445e0abfc0f6aaa1aad6da28
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-11-09 19:20:59 -0300

    Modify security configuration. Ref #11

--- src/main/java/com/tienda/project/config/SecurityConfig.java
+++ src/main/java/com/tienda/project/config/SecurityConfig.java
@@ -3,51 +3,42 @@ package com.tienda.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     private final AuthenticationProvider authenticationProvider;
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        String[] endpointsURL = {
-            "/productos/traer",
-            "/productos/{codigoProducto}",
-            "/users/editar/{idUser}",
-            "/ventas/crear",
-            "/ventas/{codigoVenta}/listaProductos/add/{codigoProducto}",
-            "/ventas/{codigoVenta}/listaProductos/del/{codigoProducto}",
-            "/ventas/{codigoVenta}",
-            "/ventas/productos/",
-            //cambiar url ,
-            "/productos/{codigoVenta}"
-        };
-
-        return http
-            .csrf(csrf -> 
-                csrf
-                .disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers(endpointsURL).permitAll()
-                .anyRequest().hasRole("ADMIN"))
-            .sessionManagement(sessionManager->
-                sessionManager 
-                  .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
-    }
+        http
+        .csrf(csrf -> 
+            csrf
+            .disable())
+        .authorizeHttpRequests(auth -> auth
+            .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+            .requestMatchers("/auth/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .sessionManagement(sessionManager->
+            sessionManager 
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }    
 }
